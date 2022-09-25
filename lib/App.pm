bf5f79b78fc6c0933955c0f2fb9e67717d63bf80
Gabor Szabo <gabor@szabgab.com>
2022-09-25 07:24:07 +0300

    perl

---
+++ lib/App.pm
@@ -0,0 +1,14 @@
+package App;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+App - Demo Perl package
+
+=cut
+
---
+++ t/app.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+
+is 1, 1, 'demo';
+
+done_testing;
+
+
